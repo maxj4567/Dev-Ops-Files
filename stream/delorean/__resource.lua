841789e10fa7fe90f6834461cb2f8f35d840f01b
MorganCALI <107836829+MorganCALI@users.noreply.github.com>
2022-06-21 22:49:01 -0400

    New civ delorean it doesn't fly

---
+++ stream/delorean/__resource.lua
@@ -0,0 +1,19 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    'vehicles.meta',
+    'handling.meta', 
+    'carvariations.meta',
+    'vehicleweapons_a10.meta',
+    'vehicleweapons_tos1.meta',
+    'weapon_.50.meta',
+    
+    
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta' 
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'WEAPONINFO_FILE' 'vehicleweapons_a10.meta'
+data_file 'WEAPONINFO_FILE' 'vehicleweapons_tos1.meta'
+data_file 'WEAPONINFO_FILE' 'weapon_.50.meta'
