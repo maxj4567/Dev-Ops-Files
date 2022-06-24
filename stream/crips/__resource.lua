5eb9b1d0ad1c15af6bcb5cf1096cb9fed822966b
max-j69 <jmax48104@gmail.com>
2022-06-24 03:09:47 -0500

    CIV EUP

---
+++ stream/crips/__resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+	accs_001_u.ydd
+	accs_001_u.ydd
+	accs_diff_001_a_uni.ytd
+	accs_diff_002_a_uni.ytd
+	
+}
+
+
+
---
+++ stream/femaleganghoodies/__resource.lua
@@ -0,0 +1,21 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+	mp_f_freemode_01^jbib_014_u.ydd
+	mp_f_freemode_01^jbib_diff_014_a_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_b_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_c_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_d_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_e_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_f_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_g_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_h_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_i_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_j_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_k_uni.ytd
+	mp_f_freemode_01^jbib_diff_014_a_uni.ytd
+	
+}
+
+
+
